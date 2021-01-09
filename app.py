116b2559005d5807084efaca3df5e65160cf3c54
Muhammad Ayman <40309064+muhammadayman97@users.noreply.github.com>
2021-01-09 21:56:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,16 @@ from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
 import nltk
-from nltk.downloader import download, download_shell 
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 import pickle
 
 # load the model from disk
-nltk.download('wordnet')
-nltk.download('stopwords')
-nltk.data.path.append('./nltk_data/')
 filename = '/home/muhammad/Desktop/new_sent/nlp_model.pkl'
 NB = pickle.load(open(filename, 'rb'))
 tfidf_vectorizer = pickle.load(open('/home/muhammad/Desktop/new_sent/transform.pkl','rb'))
 app = Flask(__name__, template_folder='template')
-RUN python -m nltk.downloader popular
 
 
 @app.route('/')
